c5c1973600f3f592f5c86b993b8f4b477048092b
Anar Seferov <1anarseferov@gmail.com>
2020-12-02 14:33:10 +0400

    new files added

---
+++ main.js
@@ -0,0 +1,12 @@
+function hit(elm) {
+    if (elm.scale_val == null) {
+        elm.scale_val = 0.8;
+    }
+    elm.scale_val = Math.round(elm.scale_val * 10) / 10;
+    if (elm.scale_val <= 0) {
+        elm.remove();
+    } else {
+        elm.style.transform = "scale(" + elm.scale_val + ")";
+    }
+    elm.scale_val -= 0.2;
+}
